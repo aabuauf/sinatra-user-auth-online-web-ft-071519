1acd9846f2635947970cea3991d3c1862fc1ad9d
Ahmed Abououf <aabuauf@yahoo.com>
2019-08-28 13:09:52 +0000

    Automatically backed up by Learn

--- db/migrate/20190828130904_create_users_table.rb
+++ db/migrate/20190828130904_create_users_table.rb
@@ -1,5 +1,9 @@
 class CreateUsersTable < ActiveRecord::Migration
   def change
-    
+    create_table :users do |t|
+      t.string :role # we will change this to t.string :role
+      t.references :user
+      t.timestamps
+    end
   end
 end
